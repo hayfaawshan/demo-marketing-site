2cec2406ba69494950524395929fadb4f21dfb66
Hayfa Awshan <hayfa.awshan@cosine.sh>
2025-03-27 16:20:43 +0000

    feat: add a loading message while TypeScript compiles
    Co-authored-by: Genie <genie@cosine.sh>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,14 @@ import "../styles/globals.css"
 import type { AppProps } from "next/app"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div className="typescript-joke">
+        <h1>While you wait for TypeScript to compile, enjoy this app!</h1>
+      </div>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
-
