3073dea2e1356b52850860a8de110d17e2e567ee
Hayfa Awshan <hayfa.awshan@cosine.sh>
2025-04-01 16:54:28 +0100

    chore: update Jest configuration for global setup and teardown
    Co-authored-by: Genie <genie@cosine.sh>

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,9 @@ const nextJest = require("next/jest");
 const createJestConfig = nextJest({ dir: "./" });
 
 const customJestConfig = {
-  testEnvironment: "jest-environment-jsdom",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  globalSetup: "<rootDir>/node_modules/@quramy/jest-prisma/global-setup",
+  globalTeardown: "<rootDir>/node_modules/@quramy/jest-prisma/global-teardown",
+  setupFilesAfterEnv: ["<rootDir>/jest.test-setup.js"],
   moduleDirectories: ["node_modules", "<rootDir>/"],
 };
 
