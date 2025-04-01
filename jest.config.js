7f80395d601bfde4946350195cffda3eed7c75a0
Genie <genie@cosine.sh>
2025-04-01 14:43:55 +0100

    chore: update Jest configuration for setup files and module directories

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,8 @@ const createJestConfig = nextJest({ dir: "./" });
 
 const customJestConfig = {
   testEnvironment: "jest-environment-jsdom",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  moduleDirectories: ["node_modules", "<rootDir>/"],
+  setupFilesAfterEnv: ["<rootDir>/lib/testing/setup.js"],
+  moduleDirectories: ["node_modules", "<rootDir>/", "lib/testing"],
 };
 
 module.exports = createJestConfig(customJestConfig);
