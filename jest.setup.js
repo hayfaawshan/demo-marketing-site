3e4d1d8e96479eb49b286532407d6908a291fce3
Hayfa Awshan <hayfa.awshan@cosine.sh>
2025-04-02 15:32:17 +0100

    feat: configure testing library and user events in jest setup
    Co-authored-by: Genie <genie@cosine.sh>

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,15 @@
 import "@testing-library/jest-dom";
+import { configure } from "@testing-library/react";
+import { setDefaultOptions } from "@testing-library/user-event";
+
+// Configure testing library
+configure({
+  testIdAttribute: "data-testid",
+  defaultHidden: true,
+  renderActiveElement: true
+});
+
+// Configure user events to run synchronously
+setDefaultOptions({
+  advanceTimers: jest.advanceTimersByTime
+});
