501dd8196ff17b2827ac749391e049c62a1bdb06
Genie <genie@cosine.sh>
2025-04-24 17:34:07 +0100

    feat: add fizzBuzz function to generate Fizz Buzz sequence

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,26 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+/**
+ * Returns the Fizz Buzz sequence from 1 to n.
+ * For multiples of 3, returns "Fizz".
+ * For multiples of 5, returns "Buzz".
+ * For multiples of both 3 and 5, returns "FizzBuzz".
+ * Otherwise, returns the number as a string.
+ */
+export function fizzBuzz(n: number): string[] {
+  const result: string[] = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 === 0) {
+      result.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      result.push("Fizz");
+    } else if (i % 5 === 0) {
+      result.push("Buzz");
+    } else {
+      result.push(i.toString());
+    }
+  }
+  return result;
+}
